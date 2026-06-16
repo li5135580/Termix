23a7cb89ad8b0122e39658983d0add5e1dac4764
Luke Gustafson <88517757+LukeGus@users.noreply.github.com>
2026-06-16 16:40:15 -0500

    Update termix.rb

--- Casks/termix.rb
+++ Casks/termix.rb
@@ -1,6 +1,6 @@
 cask "termix" do
-  version "2.3.2"
-  sha256 "cb4233c59bb1f0a0bdf981f921d84f5bed0200f4edee45c85b43de5c2f289945"
+  version "2.4.0"
+  sha256 "3cc1afc2c62ce9f40124561fb40374b34e41dcf8610b62f773a4661e9c83ca85"
 
   url "https://github.com/Termix-SSH/Termix/releases/download/release-#{version}-tag/termix_macos_universal_dmg.dmg"
   name "Termix"
